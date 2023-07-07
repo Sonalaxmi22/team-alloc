6ac4c00c552439d2836bf45312f936ec22b2268b
Sonalaxmi Udayakumar <135304659+Sonalaxmi22@users.noreply.github.com>
2023-07-07 13:19:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const container = document.getElementById("root");
+
+const root = createRoot(container); //The container const is referencing the root element 
+
+root.render(    //The render function is being called to render the app component to root element
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
